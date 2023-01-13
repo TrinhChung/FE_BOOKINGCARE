c5331dd59fa379e2470a5dba86bbd901ff023a36
TrinhChung <chungtrinh2k2@gmail.com>
2023-01-13 21:46:56 +0700

    leave room

--- src/containers/HomePage/Stream/Room.js
+++ src/containers/HomePage/Stream/Room.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Col, Row, Button } from "antd";
 import { io } from "socket.io-client";
 import Peer from "peerjs";
+import "./Room.scss";
 class Room extends Component {
   constructor(props) {
     super(props);
@@ -10,10 +11,13 @@ class Room extends Component {
       zoomId: 1,
       bookingId: 1,
       stream: null,
-      end: true,
+      ended: false,
       friendId: 1,
       voice: true,
       camera: true,
+      start: true,
+      connect: false,
+      currentCall: null,
     };
     this.myVideo = React.createRef();
     this.userVideo = React.createRef();
@@ -24,6 +28,27 @@ class Room extends Component {
       path: "peerjs",
     });
   }
+
+  disconnectVideo = async () => {
+    this.endCall();
+    this.myVideo.current.srcObject.getTracks().forEach(function (track) {
+      if (track.readyState === "live") {
+        track.stop();
+      }
+    });
+    this.props.history.push(`/home`);
+    window.location.reload();
+  };
+
+  endCall = () => {
+    if (!this.state.currentCall) return;
+    try {
+      this.state.currentCall.close();
+    } catch (error) {
+      console.log(error);
+    }
+    this.setState({ currentCall: undefined });
+  };
 
   componentDidMount() {
     if (this.props.userInfo && this.props.userInfo.id) {
@@ -40,31 +65,45 @@ class Room extends Component {
             call.answer(stream);
             call.on("stream", (remoteStream) => {
               this.userVideo.current.srcObject = remoteStream;
+              this.setState({ start: false });
             });
           });
+        this.setState({ currentCall: call });
       });
 
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((currentStream) => {
+          this.myVideo.current.srcObject = currentStream;
+          this.setState({ stream: currentStream });
+        });
       this.socket.on("user-connected", (userId) => {
-        navigator.mediaDevices
-          .getUserMedia({ video: true, audio: true })
-          .then((currentStream) => {
-            this.myVideo.current.srcObject = currentStream;
-            this.setState({ stream: currentStream });
-            const call = this.peer.call(userId, currentStream);
-            call.on("stream", (remoteStream) => {
-              this.userVideo.current.srcObject = remoteStream;
-            });
-          });
+        console.log(userId);
+        this.setState({ friendId: userId });
+        const call = this.peer.call(userId, this.state.stream);
+        call.on("stream", (remoteStream) => {
+          this.userVideo.current.srcObject = remoteStream;
+          this.setState({ start: false });
+        });
+        this.setState({ currentCall: call });
+      });
+      this.socket.on("user-disconnected", (id) => {
+        if (Number(id) === Number(this.state.friendId)) {
+          console.log(id);
+          this.setState({ ended: true });
+        }
       });
     }
   }
 
+  componentDidUpdate() {}
+
   render() {
     return (
-      <div>
+      <div style={{ backgroundColor: "black" }}>
         <div
           style={{
-            height: "93vh",
+            height: "100vh",
             position: "relative",
             display: "flex",
             justifyContent: "center",
@@ -75,72 +114,165 @@ class Room extends Component {
             autoPlay
             muted
             ref={this.myVideo}
-            style={{ position: "absolute", top: 20, right: 20, height: "20%" }}
+            className={
+              this.userVideo.current !== null ? "minVideo" : "mainVideo"
+            }
+            style={{ zIndex: 100 }}
           ></video>
-          <video
-            playsInline
-            autoPlay
-            ref={this.userVideo}
-            style={{ height: "100%" }}
-          ></video>
+          {this.state.ended !== true && (
+            <video
+              playsInline
+              autoPlay
+              ref={this.userVideo}
+              className={
+                this.userVideo.current !== null ? "mainVideo" : "minVideo"
+              }
+            ></video>
+          )}
+
+          {(this.state.start === true || this.state.ended === true) && (
+            <div
+              style={{
+                height: "100%",
+                width: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontWeight: "bold",
+                fontSize: "30px",
+                flexDirection: "column",
+                color: "white",
+              }}
+            >
+              Đối phương đang vắng mặt
+              <div>Vui lòng chờ...</div>
+            </div>
+          )}
         </div>
         <div
-          span={24}
           style={{
             padding: "10px 20px",
-            backgroundColor: "black",
-            height: "7vh",
+            height: "9vh",
+            display: "flex",
+            justifyContent: "center",
+            position: "fixed",
+            bottom: 0,
+            width: "100%",
+            backgroundColor: "transparent",
           }}
         >
           <Row
-            style={{ gap: 5, display: "flex", justifyContent: "space-between" }}
+            style={{
+              gap: 5,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
           >
-            <Col style={{ display: "flex", gap: 5 }}>
-              <Button
-                type="primary"
-                onClick={() => {
-                  const enabled =
-                    this.myVideo.current.srcObject.getAudioTracks()[0].enabled;
-                  this.setState({ voice: !enabled });
-                  this.myVideo.current.srcObject.getAudioTracks()[0].enabled =
-                    !enabled;
+            <Col
+              style={{
+                display: "flex",
+                gap: 30,
+                alignItems: "center",
+                paddingLeft: 20,
+              }}
+            >
+              <Col
+                style={{
+                  backgroundColor: "transparent",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  color: "white",
                 }}
               >
-                {this.state.voice ? (
-                  <i className="fa fa-microphone"></i>
-                ) : (
-                  <i className="fa fa-microphone-slash"></i>
-                )}
-              </Button>
-              <Button
-                type="primary"
-                onClick={() => {
-                  const enabled =
-                    this.myVideo.current.srcObject.getVideoTracks()[0].enabled;
-                  this.setState({ camera: !enabled });
-                  this.myVideo.current.srcObject.getVideoTracks()[0].enabled =
-                    !enabled;
-                  console.log(enabled);
+                <Button
+                  type="primary"
+                  onClick={() => {
+                    const enabled =
+                      this.myVideo.current.srcObject.getAudioTracks()[0]
+                        .enabled;
+                    this.setState({ voice: !enabled });
+                    this.myVideo.current.srcObject.getAudioTracks()[0].enabled =
+                      !enabled;
+                  }}
+                  style={{
+                    backgroundColor: "transparent",
+                    fontSize: 20,
+                    textAlign: "center",
+                  }}
+                >
+                  {this.state.voice ? (
+                    <i className="fa fa-microphone"></i>
+                  ) : (
+                    <i className="fa fa-microphone-slash"></i>
+                  )}
+                </Button>
+                <Col>Muted</Col>
+              </Col>
+
+              <Col
+                style={{
+                  backgroundColor: "transparent",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  color: "white",
                 }}
               >
-                {this.state.camera ? (
-                  <i className="fa fa-video-camera"></i>
-                ) : (
-                  <i className="fas">&#xf4e2;</i>
-                )}
-              </Button>
-            </Col>
-            <Col>
-              <Button
-                type="primary"
-                danger
-                onClick={() => {
-                  this.props.history.push(`/home`);
-                  window.location.reload();
+                <Button
+                  type="primary"
+                  onClick={() => {
+                    const enabled =
+                      this.myVideo.current.srcObject.getVideoTracks()[0]
+                        .enabled;
+                    this.setState({ camera: !enabled });
+                    this.myVideo.current.srcObject.getVideoTracks()[0].enabled =
+                      !enabled;
+                    console.log(enabled);
+                  }}
+                  style={{ backgroundColor: "transparent", fontSize: 20 }}
+                >
+                  {this.state.camera ? (
+                    <i className="fa fa-video-camera"></i>
+                  ) : (
+                    <i className="fas">&#xf4e2;</i>
+                  )}
+                </Button>
+                <Col>Start Camera</Col>
+              </Col>
+              <Col
+                style={{
+                  backgroundColor: "transparent",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  color: "white",
                 }}
               >
-                Kết thúc
-              </Button>
+                <Button
+                  type="primary"
+                  onClick={() => {}}
+                  style={{ backgroundColor: "transparent", fontSize: 20 }}
+                >
+                  <i className="fas">&#xf27a;</i>
+                </Button>
+                <Col>Chat</Col>
+              </Col>
+              <Col>
+                <Button
+                  type="primary"
+                  danger
+                  onClick={() => {
+                    this.disconnectVideo();
+                  }}
+                >
+                  <i className="fas fa-phone-slash"></i>
+                </Button>
+              </Col>
             </Col>
           </Row>
         </div>
